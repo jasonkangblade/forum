40e027d2d58d465a58b59d0be672c763af618cd2
root <root@localhost.localdomain>
2016-08-04 23:33:45 +0800

    20160804 23:33

--- views.py
+++ views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from block.models import Block
 
 def index(request):
-    block_infos=[{"name":"运维专区","desc":"运维学习讨论区","manager":"admin"},
-            {"name":"Django专区","desc":"Django学习讨论区","manager":"admin"},
-            {"name":"部落建设","desc":"部落建设事宜","manager":"admin"}]
+    block_infos=Block.objects.all().order_by("id")
+   # Rplace by DB***************************************************************# 
+   # block_infos=[{"name":"运维专区","desc":"运维学习讨论区","manager":"admin"},#
+   #         {"name":"Django专区","desc":"Django学习讨论区","manager":"admin"}, #
+   #         {"name":"部落建设","desc":"部落建设事宜","manager":"admin"}]       #
+   #****************************************************************************#
     return render(request,"index.html",{"blocks":block_infos})
