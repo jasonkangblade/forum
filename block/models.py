7616aa083d27d1d0bf7739dca803868a34120adf
root <root@localhost.localdomain>
2016-08-05 16:46:49 +0800

    20160805 16:46

--- block/models.py
+++ block/models.py
@@ -4,3 +4,9 @@ class Block(models.Model):
     name=models.CharField(u"板块名称",max_length=100)
     desc=models.CharField(u"板块描述",max_length=100)
     manager_name=models.CharField(u"管理员ID",max_length=100)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name="板块"
+        verbose_name_plural="板块"
